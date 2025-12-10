0abbf29013cbc91df608f0b6fbc651723a7118fe
Tsito2 <92148393+Tsito2@users.noreply.github.com>
2025-12-10 14:02:29 +0100

    Copy static assets during build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -30,6 +31,14 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'assets'),
+          to: path.resolve(__dirname, 'dist', 'assets'),
+        },
+      ],
     }),
   ],
 };
